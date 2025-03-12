e82b137c016a7728d3b06bd8d08be9ced488656c
arun-kumar <arunthangadurai2006)>
2025-03-12 15:14:11 +0530

    this is my first program

---
+++ variable/MyOrder.java
@@ -0,0 +1,7 @@
+public class MyOrder {
+    public static void main(String[] args) {
+        int num1 = 5, num2 = 7;
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
