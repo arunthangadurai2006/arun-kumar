01e6ffb95578c8948e50abfc2158d0c3c4dce9e5
arun-kumar <arunthangadurai2006)>
2025-03-13 13:50:50 +0530

    array

---
+++ array/Array.java
@@ -0,0 +1,10 @@
+public class Array{
+    public static void main (String args[]){
+      int  array[]={10,20,30,40,50};
+    
+    System.out.println(array[0]);
+    System.out.println(array[1]);
+    System.out.println(array[2]);
+    System.out.println(array[3]);
+    }
+}
