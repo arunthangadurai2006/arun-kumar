cabcc2d6d616332a6b56608f8d72da27b9aa7399
arun-kumar <arunthangadurai2006)>
2025-03-13 11:51:09 +0530

    positive or negstive)

---
+++ positive/Posneg.java
@@ -0,0 +1,11 @@
+public class Posneg {
+    public static void main(String[] args) {
+        int number = 18;
+
+        if (number < 0) {
+            System.out.println("its is negative number");
+        } else {
+            System.out.println("its is positive number");
+        }
+    }
+}
