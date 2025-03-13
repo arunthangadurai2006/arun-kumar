58286af8f57cdf6651abce8de59cef21594fcabb
arun-kumar <arunthangadurai2006)>
2025-03-13 12:07:11 +0530

    neutral

---
+++ neutral/neutral.java
@@ -0,0 +1,14 @@
+public class neutral {
+    public static void main(String[] args) {
+        int number = 18;
+
+        if (number < 0) {
+            System.out.println("its is negative number");
+        } 
+        else if(number==0){
+            System.out.println("its is neutral");
+        }
+        else
+        System.out.println("its is positive number");
+    }
+}
