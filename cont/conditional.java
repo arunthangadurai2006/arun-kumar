f083d6b3f3359d041a393b7cf4641190ca5b6bc1
arun-kumar <arunthangadurai2006)>
2025-03-13 11:09:51 +0530

    conditional

---
+++ cont/conditional.java
@@ -0,0 +1,11 @@
+public class conditional {
+    public static void main(String[] args) {
+        int a = 20, b = 5;
+
+        // Comparing values
+        System.out.println("Is a less than b " + (a < b));
+        System.out.println("Is a greater than b " + (a > b));
+        System.out.println("Is a greater than or equal to b " + (a >= b));
+        System.out.println("Is a less than or equal to b " + (a <= b));
+    }
+}
