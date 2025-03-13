4638a4f8083adfdb8ff9e00a13eadd5af5fef012
arun-kumar <arunthangadurai2006)>
2025-03-13 10:54:25 +0530

    arithmetic

---
+++ Arith/Arithmetic.java
@@ -0,0 +1,11 @@
+
+public class Arithmetic{
+    public static void main(String[] args) {
+        int a = 20, b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
+
