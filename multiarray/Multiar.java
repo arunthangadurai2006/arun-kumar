0aa5aabad4244f693f84f44b04e7052b566a4d31
arun-kumar <arunthangadurai2006)>
2025-03-13 14:10:00 +0530

    multiple array

---
+++ multiarray/Multiar.java
@@ -0,0 +1,9 @@
+public class Multiar{
+    public static void main (String args[]){
+      int  array[][]={{1,5,3,8,6},{2,4,5,6,8},{7,6,5,7,4}};
+    System.out.println(array[0][1]);
+    System.out.println(array[1][2]);
+    System.out.println(array[2][0]);
+    System.out.println(array[1][2]);
+    }
+}
