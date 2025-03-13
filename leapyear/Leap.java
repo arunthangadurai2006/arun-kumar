a9e424fc1c5d6f00402c7c6b6f24b82d9c09f9ca
arun-kumar <arunthangadurai2006)>
2025-03-13 14:43:59 +0530

    leapyear

---
+++ leapyear/Leap.java
@@ -0,0 +1,18 @@
+class Leap {
+    public static void main(String[] args) {
+        int year = 2025;
+        boolean isLeapYear = false;
+
+        if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                isLeapYear = true;
+            }
+        }
+
+        if (isLeapYear) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+}
