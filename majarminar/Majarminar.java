c464d986cc43883bf8a7ede04097618f9eb2cc1b
arun-kumar <arunthangadurai2006)>
2025-03-13 11:42:15 +0530

    major or minor

---
+++ majarminar/Majarminar.java
@@ -0,0 +1,11 @@
+public class Majarminar {
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age < 18) {
+            System.out.println("you are minor");
+        } else {
+            System.out.println("major");
+        }
+    }
+}
