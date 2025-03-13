3017036d097f5b6e91a8fc61bb984fd02c8499c1
arun-kumar <arunthangadurai2006)>
2025-03-13 11:21:03 +0530

    if conditional

---
+++ if oper/If.java
@@ -0,0 +1,9 @@
+public class If {
+    public static void main(String[] args) {
+        int number = 10;
+
+        if (number > 5) {
+            System.out.println("The number is greater than 5.");
+        }
+    }
+}
