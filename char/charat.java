6bb47eee5c8739aa62910ccc047ea0b1611ead1a
arun-kumar <arunthangadurai2006)>
2025-03-14 11:42:38 +0530

    charat

---
+++ char/charat.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class charat {
+    public static void main(String[] args) {
+        String name = "hello";
+        System.out.println( name.charAt(0));
+    }
+}
