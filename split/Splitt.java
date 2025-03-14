1c50677ea76b74788d78a42bf8fd0386db28f498
arun-kumar <arunthangadurai2006)>
2025-03-14 11:58:12 +0530

    split

---
+++ split/Splitt.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Splitt {
+    public static void main(String[] args) {
+        String name = "hello,world";
+        String names[] = name.split(",");
+        System.out.println(names[0]); 
+    }
+}
