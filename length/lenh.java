20091bb00fabbfdbb77d369b0ea672cb14c7a688
arun-kumar <arunthangadurai2006)>
2025-03-14 12:14:46 +0530

    lenght

---
+++ length/lenh.java
@@ -0,0 +1,7 @@
+public class lenh {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        int length = message.length(); 
+        System.out.println("The length of the string is: " + length);
+    }
+}
