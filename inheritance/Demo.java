81b1794081b79985ee93d99f533a0be59adade30
arun-kumar <arunthangadurai2006)>
2025-03-14 13:48:20 +0530

    inheritance

---
+++ inheritance/Demo.java
@@ -0,0 +1,18 @@
+class Parent {
+    public void add() {
+        System.out.println("hello world");
+    }
+}
+
+class Child extends Parent {
+    void display() {
+        add(); 
+    }
+}
+
+public class Demo { 
+    public static void main(String[] args) {
+        Child obj = new Child();
+        obj.display(); 
+    }
+}
