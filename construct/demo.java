6ac5b03f40b7f8f81b9cf44c3fcda52489ba461a
arun-kumar <arunthangadurai2006)>
2025-03-15 11:09:23 +0530

    construct

---
+++ construct/demo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class demo{
+    demo(){
+        System.out.print("arun");
+    }
+    public static void main(String args[]){
+    
+   demo obi = new demo();
+    }
+}
